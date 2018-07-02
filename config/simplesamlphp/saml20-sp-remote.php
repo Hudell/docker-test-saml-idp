9ef03d9748f4cfb273996acbaa667bbd73ce28ef
Pierre <pierre.lehnen@rocket.chat>
2018-07-02 17:42:56 -0300

    Added config for basic Rocket.Chat integration

--- config/simplesamlphp/saml20-sp-remote.php
+++ config/simplesamlphp/saml20-sp-remote.php
@@ -9,3 +9,31 @@ $metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID')] = array(
     'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'),
     'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE'),
 );
+
+$metadata['http://localhost:3000/_saml/metadata/test-sp'] = array (
+  'entityid' => 'http://localhost:3000/_saml/metadata/test-sp',
+  'contacts' => 
+  array (
+  ),
+  'metadata-set' => 'saml20-sp-remote',
+  'AssertionConsumerService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+      'Location' => 'http://localhost:3000/_saml/validate/test-sp',
+      'index' => 1,
+      'isDefault' => true,
+    ),
+  ),
+  'SingleLogoutService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'http://localhost:3000/_saml/logout/test-sp/',
+      'ResponseLocation' => 'http://localhost:3000/_saml/logout/test-sp/',
+    ),
+  ),
+  'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+);
